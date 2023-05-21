aafabb6db9e7700e0e4a7839b164af7618fde6de
Vishaal Yalamanchali <vishaal.yalaman@gmail.com>
2023-05-21 00:59:41 -0500

    push for amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "voicefrontend96b9006b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { Amplify } from "aws-amplify";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import VoiceRegistration from "./voiceModule/voiceRegistration.js";
+import VoiceVerification from "./voiceModule/voiceVerification.js";
+import NavBar from "./assets/NavBar.js";
+import Home from "./Home.js";
+import "@aws-amplify/ui-react/styles.css";
+import "bulma/css/bulma.css";
+
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
+  return (
+    <nav>
+      <NavBar user={user} signOut={signOut} />
+
+      <div className="App">
+        <React.StrictMode>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home user={user} />} />
+              <Route
+                path="/registration"
+                element={<VoiceRegistration user={user} />}
+              />
+              <Route
+                path="/verification"
+                element={<VoiceVerification user={user} />}
+              />
+            </Routes>
+          </BrowserRouter>
+        </React.StrictMode>
+      </div>
+    </nav>
+  );
+}
+
+export default withAuthenticator(App);
